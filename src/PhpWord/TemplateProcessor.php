a7b5f82df9fd03560d8cc13409c964a5e400fa38
Premixedpie <zmatthewsmail@gmail.com>
2015-08-10 15:25:00 -0700

    Adding setImageValue

--- src/PhpWord/TemplateProcessor.php
+++ src/PhpWord/TemplateProcessor.php
@@ -452,4 +452,25 @@ class TemplateProcessor
 
         return substr($this->temporaryDocumentMainPart, $startPosition, ($endPosition - $startPosition));
     }
+    
+    /**
+     * Set a new image
+     *
+     * @param string $search
+     * @param string $replace
+     */
+    public function setImageValue($search, $replace)
+    {
+        // Sanity check
+        if (!file_exists($replace))
+        {
+            return;
+        }
+
+        // Delete current image
+        $this->zipClass->deleteName('word/media/' . $search);
+
+        // Add a new one
+        $this->zipClass->addFile($replace, 'word/media/' . $search);
+    }
 }
